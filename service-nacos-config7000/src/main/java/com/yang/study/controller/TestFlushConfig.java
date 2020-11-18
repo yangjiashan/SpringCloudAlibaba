16fc2ba4bae2257672c3ffb31af14c2de834fb68
yjiashan <yjs@123456>
2020-11-18 18:21:25 +0800

    添加nacos测试代码

---
+++ service-nacos-config7000/src/main/java/com/yang/study/controller/TestFlushConfig.java
@@ -0,0 +1,17 @@
+package com.yang.study.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestFlushConfig {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping("/testString")
+    public String testString(){
+        return "1";
+    }
+}
