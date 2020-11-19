93f7225440f56eb7339249a5770fb3a0e27849da
yjiashan <18396129028@139.com>
2020-11-20 00:01:17 +0800

    添加nacos配置自动刷新

--- service-nacos-config7000/src/main/java/com/yang/study/controller/TestFlushConfig.java
+++ service-nacos-config7000/src/main/java/com/yang/study/controller/TestFlushConfig.java
@@ -1,10 +1,12 @@
 package com.yang.study.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestFlushConfig {
 
     @Value("${server.port}")
@@ -12,6 +14,6 @@ public class TestFlushConfig {
 
     @RequestMapping("/testString")
     public String testString(){
-        return "1";
+        return serverPort;
     }
 }
