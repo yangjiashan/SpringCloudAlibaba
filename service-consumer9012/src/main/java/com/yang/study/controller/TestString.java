2fb10a4c691030c2e9405ccac3a0581532f29428
yjiashan <yjs@123456>
2020-11-20 18:33:10 +0800

    添加sentinel持久化配置

--- service-consumer9012/src/main/java/com/yang/study/controller/TestString.java
+++ service-consumer9012/src/main/java/com/yang/study/controller/TestString.java
@@ -1,5 +1,7 @@
 package com.yang.study.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.yang.study.service.TestFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,16 @@ public class TestString {
     private TestFeignService testFeignService;
 
     @RequestMapping("/testString")
+    @SentinelResource(value = "getSentinelConfigByNacos", blockHandler = "getSentinelConfigByNacosBlock")
     public String testString() {
         return testFeignService.testString();
+    }
+
+    public String getSentinelConfigByNacosBlock(BlockException be) {
+        return "getSentinelConfigByNacos 接口访问已经达到上限了";
+    }
+
+    public String getSentinelConfigByNacosFallback() {
+        return "访问频率太高，执行降级";
     }
 }
